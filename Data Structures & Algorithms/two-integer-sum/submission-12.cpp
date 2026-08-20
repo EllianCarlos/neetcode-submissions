4e541a43320a1812cb96aed150316953040293d6
Ellian Carlos <elliancarlos@gmail.com>
2026-08-19 23:45:23 -0300

    Add: two-integer-sum - submission-12

---
+++ Data Structures & Algorithms/two-integer-sum/submission-12.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> missingToTarget;
+
+        for (int i = 0; i < nums.size(); ++i) {
+            auto curr = missingToTarget.find(nums[i]);
+            if (curr != missingToTarget.end()) {
+                return {curr->second, i};
+            }
+
+            missingToTarget[target - nums[i]] = i;
+        }
+
+        return {-1,-1};
+    }
+};
